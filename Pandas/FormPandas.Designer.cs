fa3a199bb6047e887b3954246b152f4faebacb33
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-08 16:00:47 +0200

    Add project files.

---
+++ Pandas/FormPandas.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Pandas
+{
+    partial class FormPandas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnStartStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(839, 458);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnStartStop
+            // 
+            this.BtnStartStop.Location = new System.Drawing.Point(12, 12);
+            this.BtnStartStop.Name = "BtnStartStop";
+            this.BtnStartStop.Size = new System.Drawing.Size(219, 51);
+            this.BtnStartStop.TabIndex = 1;
+            this.BtnStartStop.Text = "Start / Stop";
+            this.BtnStartStop.UseVisualStyleBackColor = true;
+            this.BtnStartStop.Click += new System.EventHandler(this.BtnStartStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormPandas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 839);
+            this.Controls.Add(this.BtnStartStop);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormPandas";
+            this.Text = "Pandas";
+            this.Load += new System.EventHandler(this.FormPandas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnStartStop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Pandas/FormPandas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Pandas
+{
+    public partial class FormPandas : Form
+    {
+        public FormPandas()
+        {
+            InitializeComponent();
+        }
+
+        Image[] images = new Image[226];
+        int count = 0;
+
+        private void FormPandas_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 225; i++)
+            {
+                images[i] = Image.FromFile(Application.StartupPath + @"\Images\panda" + i + ".png");
+            }
+
+            //set 1st image as initial
+            pictureBox1.Image = images[0];
+            //set pictureBox the same size as the image
+            pictureBox1.Size = pictureBox1.Image.Size;
+        }
+
+        private void BtnStartStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image = images[count];
+            count++;
+            if (count > 225)
+                count = 0;
+        }
+    }
+}
